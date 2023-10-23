afdbb13812c1192841cf25fa6ec6e2f1b0934831
Mateus <mateus@dooly.ai>
2023-10-23 01:48:52 -0600

    Add message option

--- commands/all/sarcasm.js
+++ commands/all/sarcasm.js
@@ -3,9 +3,16 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("sarcasm")
-    .setDescription("Replies with Pong!"),
-  async execute(interaction) {
-    await interaction.reply("Pong!");
-    console.log(Message.content);
-  },
+    .setDescription("Replies with message!")
+    .addStringOption(option =>
+      option
+        .setName("message")
+        .setDescription("The text to convert to sarcasm.")
+        .setRequired(true)
+    ),
+    async execute(interaction) {
+      const message = interaction.options.getString("text");
+      console.log('message', message);
+      await interaction.reply(message);
+    },
 };
